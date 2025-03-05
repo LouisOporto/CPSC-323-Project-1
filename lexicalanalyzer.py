cb2fe515815c891882f40e2d5bdb858f7a9412ae
LouisOporto <louisoprto042@csu.fullerton.edu>
2025-03-05 14:30:15 -0800

    Formatting output without implementation

--- lexicalanalyzer.py
+++ lexicalanalyzer.py
@@ -1,4 +1,9 @@
+#Import any necessary libraries
 
+
+
+# Define the token types
+# KEYWORD, SEPERATOR, IDENTIFIER, OPERATOR, INTEGER, UNKNOWN, END
 token_type = {
     'KEYWORD': 0,
     'SEPERATOR': 1,
@@ -6,24 +11,36 @@ token_type = {
     'OPERATOR': 3,
     'INTEGER': 4,
     'UNKNOWN': 5,
+    'END': 6
 }
 
+# Define token structure
+class Token:
+    def __init__(self, token_type, lexeme):
+        self.token_type = token_type
+        self.lexeme = lexeme
+
+    def __str__(self):
+        return f"Token Type: {self.token_type}, Lexeme: {self.lexeme}"
+    
+
+# Define the lexical analysis function
 def lexical_analysis(input: str):
     """ Given the file text as input read the text line by line and return the identified token types"""
     white_space = ' '
     lexme = ''
-
+    
+    print("Set of lexemes and tokens (<lexemes> = <tokens>)")
+    print("\"")
     for i, char in enumerate(input):
         if char != white_space:
             lexme += char
             if (i + 1 < len(input)): 
                 if input[i + 1] == white_space:
-                    print(lexme)
+                    print(f"{lexme} = <seperator>")
                     lexme = ''
-    print (lexme) # Final lexme before end of file
-                
-        
-
+    print (f"{lexme} = <seperator>") # Final lexme before end of file
+    print("\"")
 
 if __name__ == "__main__":
     '''Run lexical program on user inputted file'''
@@ -36,4 +53,4 @@ if __name__ == "__main__":
 
     print("Running lexical analyzer...")
     lexical_analysis(full_text)
-    print("Lexical analysis complete")
+    print("Lexical analysis complete!")
